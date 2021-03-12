2aeebae497f5a90ec084e07c3f6d9b76308dfeb1
Kranthi <Kranthi@DESKTOP-BGDPM3C>
2021-03-12 22:14:23 +0530

    [MASTER97]: Added Reverse String Class and program

--- src/main/java/com/javaprograms/demo/ReverseString_01.java
+++ src/main/java/com/javaprograms/demo/ReverseString_01.java
@@ -1,5 +1,28 @@
 package com.javaprograms.demo;
 
 public class ReverseString_01 {
+	
+	public static void main(String[] args) {
+//		Kranthi method
+//		String a = "sp software private limited";
+//		
+//		
+//		int size = a.length();
+//		for(int i=size-1 ; i>=0 ;i--)
+//		{
+//			char ch =  a.charAt(i);
+//			System.out.print(ch);
+//		}
+		
+//		############ Method - 1 ##########
+		
+		String name = "sp software private limited";
+		byte[] nameAsBytes = name.getBytes();
+		byte[] result = new byte[nameAsBytes.length];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = nameAsBytes[result.length-1];
+		}
+		System.out.println(new String(result));
+	}
 
 }
